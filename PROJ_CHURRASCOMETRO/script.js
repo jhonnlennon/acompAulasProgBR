ddf3773b9c11cdfe3eea83312fb4e571e266e0e1
jhonnlennon <jhonn1990@hotmail.com>
2021-11-09 22:40:02 -0300

    Treinamento Javascript

--- PROJ_CHURRASCOMETRO/script.js
+++ PROJ_CHURRASCOMETRO/script.js
@@ -1,3 +1,4 @@
+
 function calcular() {
     /* Nossa lógica */
     /*carne - 400g/people, if + 6hours 650g */
@@ -10,18 +11,36 @@ function calcular() {
     let inputCriancas = document.getElementById("criancas");
     let inputDuracao = document.getElementById("duracao");
 
-    let adultos = inputAdultos.value;
+    var adultos = inputAdultos.value;
     let criancas = inputCriancas.value;
     let duracao = inputDuracao.value;
 
     if (duracao > 6 && criancas > 0) {
-        ((adultos *= 650) * 0.5)
-        console.log(adultos);
+        let resultadoCarne = ((adultos * 650) + ((criancas * 650) / 2)); //resultado carne
+        let resultadoRefrigerante = criancas * 1500; //resultado refri
+        let resultadoCerveja = adultos * 2000; //resultado cerveja 
+        console.log(resultadoCarne, resultadoCerveja, resultadoRefrigerante);
+    } else if (duracao <= 6 && criancas > 0) {
+        let resultadoCarne = ((adultos * 400) + ((criancas * 400) / 2)); //resultado carne
+        let resultadoRefrigerante = criancas * 1000; //resultado refri
+        let resultadoCerveja = adultos * 1200;//resultado cerveja 
+        console.log(resultadoCarne, resultadoCerveja, resultadoRefrigerante);
     } else if (duracao > 6) {
-        ((adultos *= 650) * 0.5)
-        console.log(adultos);
+        let resultadoCarne = adultos * 650; //resultado carne
+        let resultadoCerveja = adultos * 2000; //resultado cerveja 
+        console.log(resultadoCarne, resultadoCerveja);
+    } else if (duracao <= 6) {
+        let resultadoCarne4 = adultos * 400; //resultado carne
+        let resultadoCerveja4 = adultos * 1200; //resultado cerveja
+        return lancar() + resultadoCarne4
+
+        console.log(resultadoCarne4, resultadoCerveja4);
+
     } else {
-        adultos *= 400
-        console.log(adultos);
+        alert("Algo deu errado, verifique se tem algum número negativo!")
     }
+}
+function lancar() {
+    let liCarner = document.getElementById("carne");
+    liCarner.innerHTML = "A quantidade mínima de carne é: "
 }
